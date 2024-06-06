86c504cd8421d584c9b1369680be30f8426ce1d1
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2024-06-05 21:17:07 -0400

    add `.gitignore` to `map1` folder and remove now ignored folders

--- map1/out/server/gameplay/2p/setup.lua
+++
@@ -1,6 +0,0 @@
--- Compiled with roblox-ts v2.3.0
-local function start2pGame(players)
-end
-return {
-	start2pGame = start2pGame,
-}
--- map1/out/server/system_message.lua
+++
@@ -1,9 +0,0 @@
--- Compiled with roblox-ts v2.3.0
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local function triggerSystemMessage(message)
-	ReplicatedStorage.SystemChatEvent:FireAllClients(message)
-	print("Sending system message: " .. message)
-end
-return {
-	triggerSystemMessage = triggerSystemMessage,
-}
