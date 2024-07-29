230c244a62670b9416b200007b415a27b4f6419a
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2024-07-29 11:13:04 -0400

    change to using topbar for role selection ui

--- lobby/src/client/ui.client.ts
+++ lobby/src/client/ui.client.ts
@@ -1,6 +1,39 @@
 import Roact from "@rbxts/roact";
 import { roleSelect } from "./elements/role_select";
+import Icon from "shared/Icon";
 
 const Players = game.GetService("Players");
+const ReplicatedStorage = game.GetService("ReplicatedStorage");
 
-Roact.mount(roleSelect, Players.LocalPlayer.FindFirstChildOfClass("PlayerGui"));
+// Roact.mount(roleSelect, Players.LocalPlayer.FindFirstChildOfClass("PlayerGui"));
+
+const ghostButton = new Icon().setLabel("Ghost");
+const luigiButton = new Icon().setLabel("Luigi");
+const cancelButton = new Icon().setLabel("No Preference");
+
+ghostButton.selected.Connect(function () {
+  sayPrefersTeam("Prefers Ghost");
+  luigiButton.deselect();
+  cancelButton.deselect();
+});
+luigiButton.selected.Connect(function () {
+  sayPrefersTeam("Prefers Luigi");
+  ghostButton.deselect();
+  cancelButton.deselect();
+});
+cancelButton.selected.Connect(function () {
+  sayPrefersTeam("No Preference");
+  ghostButton.deselect();
+  luigiButton.deselect();
+});
+
+new Icon()
+  .setLabel("Select Role")
+  .modifyTheme(["Dropdown", "MaxIcons", 3])
+  .setDropdown([ghostButton, luigiButton, cancelButton]);
+
+cancelButton.select();
+
+function sayPrefersTeam(team: string) {
+  ReplicatedStorage.PrefersTeamEvent.FireServer(team);
+}
